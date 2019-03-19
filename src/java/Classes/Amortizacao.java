b5dcaa6a741b503cadb351af8af2de95240caf35
Alexander Santos <alexanderferreira@gmail.com>
2019-03-18 23:06:57 -0300

    Classe java para realizar o cálculo de amortização.

---
+++ src/java/Classes/Amortizacao.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author alexander
+ */
+public class Amortizacao {
+    double pmt = 0;
+    public Double CalcPrestPrice(double vr, double np, double jr){
+        double d1 = Math.pow((1+(jr/100)),np);
+        
+        pmt = vr / ((d1-1) / (d1*(jr/100)) );
+        
+        return pmt;
+    }
+    public Double CalcPrestAmeric(double vr, double jr){
+        pmt = vr * (jr/100);
+        
+        return pmt;
+    }
+    public Double CalcPrestConst(double vr, double np){
+        pmt = vr / np;
+        
+        return pmt;
+    }
+    public String createTable(double vr, double np, double jr, double pmt, String a){
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        double juros = 0;
+        double amortizacao = 0;
+        double saldo = vr;
+        
+        //somatorias
+        double jurosSoma = 0;
+        double amortizacaoSoma = 0;
+        double prestacaoSoma = 0;
+        
+        String table = "<table class='table table-bordered container-fluid' style='width: 800px;'><tr>"
+                + "<th>Nº</th>"
+                + "<th>Prestação</th>"
+                + "<th>Juros</th>"
+                + "<th>Amortização</th>"
+                + "<th>Saldo Devedor</th>"
+                + "<tr>"
+                + "<tr>"
+                + "<td>"+0+"</td>"
+                + "<td>"+df.format(0)+"</td>"
+                + "<td>"+df.format(juros)+"</td>"
+                + "<td>"+df.format(amortizacao)+"</td>"
+                + "<td>"+df.format(saldo)+"</td>"
+                + "</tr>";
+        if (a=="const")
+            amortizacao = pmt;
+        for(int j = 1; j <= np; j++)
+        {
+            if (a=="americ" && j==np)
+                pmt = saldo + pmt;
+            
+            juros = (saldo*(jr/100));
+            
+            if (a=="const")
+                pmt = amortizacao+juros;
+            
+            prestacaoSoma = prestacaoSoma + pmt;
+            jurosSoma = jurosSoma + juros;
+            if (a=="americ" && j==np)
+                amortizacao=saldo;
+            else if (a=="price")
+                amortizacao = pmt - juros;
+            amortizacaoSoma = amortizacaoSoma + amortizacao;
+            saldo = saldo - amortizacao;
+            
+            if (saldo<0)
+                saldo=0;
+            table = table + "<tr>"
+                    + "<td>"+j+"</td>"
+                    + "<td>"+df.format(pmt)+"</td>"
+                    + "<td>"+df.format(juros)+"</td>"
+                    + "<td>"+df.format(amortizacao)+"</td>"
+                    + "<td>"+df.format(saldo)+"</td>"
+                    + "</tr>";
+        }
+        table = table +"<tr>"
+                    + "<td>TOTAL</td>"
+                    + "<td>"+df.format(prestacaoSoma)+"</td>"
+                    + "<td>"+df.format(jurosSoma)+"</td>"
+                    + "<td>"+df.format(amortizacaoSoma)+"</td>"
+                    + "<td>"+df.format(saldo)+"</td>"
+                    + "</tr>" +"</table>";
+        return table;
+    }
+}
--- web/saa.jsp
+++ web/saa.jsp
@@ -3,7 +3,8 @@
     Created on : 13/03/2019, 16:11:12
     Author     : Mulero
 --%>
-
+<%@page import="java.text.DecimalFormat" %>
+<%@page import="Classes.Amortizacao" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -12,11 +13,59 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Amortização Americana</title>
     </head>
+    <%
+        Amortizacao saa = new Amortizacao();
+        double vr = 0;
+        double jr = 0;
+        double np = 0;
+        try {
+            vr = Double.parseDouble(request.getParameter("vr"));
+            jr = Double.parseDouble(request.getParameter("jr"));
+            np = Double.parseDouble(request.getParameter("np"));
+        } catch (Exception ex) {
+
+        }
+    %>
      <body>
         <%@include file="WEB-INF/jspf/header.jspf" %>
         <h1>Sistema de Amortização Americana</h1>
         <%@include file="WEB-INF/jspf/menu.jspf" %>
+        <form class="container-fluid format_tabela format_form">
 
+                <div class="form-group form-horizontal">
+                    <label class="col-sm-4 control-label" for="valor">Valor</label>
+                    <div class="input-group">
+                        <input class="form-control" type="number" name="vr" value=""  step="any"/>
+                    </div>
+                </div>
+
+                <div class="form-group form-horizontal">
+                    <label class="col-sm-4 control-label" for="prestacao">Número de prestações</label>
+                    <div class="input-group">
+<!--                        <div class="input-group-addon">&nbsp;&nbsp;&nbsp;&nbsp;</div>-->
+                        <input class="form-control" type="number" name="np" value="" />
+                    </div>      
+                </div>
+
+                <div class="form-group form-horizontal">
+                    <label class="col-sm-4 control-label" for="juros">Taxa de Juros (% ao mês)</label>
+                    <div class="input-group">
+<!--                        <div class="input-group-addon">&nbsp;%</div>-->
+                        <input class="form-control" type="number" name="jr" value=""  step="any"/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-4">
+                        <input class="btn btn-primary" type="submit"  name="btnCalc" value="Calcular"/>
+                    </div>
+                </div>
+            </form>
+            <% if (request.getParameter("btnCalc") != null) { %>
+                <div class="container-fluid format_tabela"><% DecimalFormat dv = new DecimalFormat("#,##0.00");
+                out.print("Valor da Prestação R$ " + dv.format(saa.CalcPrestAmeric(vr, jr)));%></div>
+                <%out.print(saa.createTable(vr, np, jr, saa.CalcPrestAmeric(vr, jr), "americ"));
+            }%>
         <%@include file="WEB-INF/jspf/footer.jspf" %>
     </body>
 </html>
